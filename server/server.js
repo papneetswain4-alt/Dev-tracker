fc6c090550240ee98f7e7d61c3041009d4262e2e
papneetswain4-alt <papneetswain4@gmail.com>
2026-02-08 03:15:41 +0530

    PORT change

--- server/server.js
+++ server/server.js
@@ -28,5 +28,6 @@ app.get("/api/protected", authMiddleware, (req, res) => {
 app.use("/api/logs", logRoutes);
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
