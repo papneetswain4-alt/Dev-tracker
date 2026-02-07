742b27cdbd7d3259f3d64a8e98c5a35bba5997ed
papneetswain4-alt <papneetswain4@gmail.com>
2026-02-08 03:24:02 +0530

    connect frontend to live backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://dev-tracker-api-0f6x.onrender.com"
 });
 
 API.interceptors.request.use((req) => {
