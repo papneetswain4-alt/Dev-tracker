6e9db546d42e016c2cc14a19d3ffcb63209ad3ce
papneetswain4-alt <papneetswain4@gmail.com>
2026-02-08 11:35:14 +0530

    #

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://dev-tracker-api-0f6x.onrender.com"
+  baseURL: "https://dev-tracker-api-0f6x.onrender.com/api"
 });
 
+// Attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-
   if (token) {
-    req.headers.Authorization = token;
+    req.headers.Authorization = `Bearer ${token}`;
   }
-
   return req;
 });
 
